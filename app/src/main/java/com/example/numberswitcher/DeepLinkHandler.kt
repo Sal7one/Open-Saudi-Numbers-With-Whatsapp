698c5be6298f1263df22edf83a28aafc932b1a0c
Saleh Alanazi <alanazi.saleh.77@gmail.com>
2022-02-13 02:39:10 +0300

    Added icon and removed unused activity

--- app/src/main/java/com/example/numberswitcher/DeepLinkHandler.kt
+++ app/src/main/java/com/example/numberswitcher/DeepLinkHandler.kt
@@ -2,27 +2,36 @@ package com.example.numberswitcher
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class DeepLinkHandler : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val text = intent
             .getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
-
         numberHandler(text)
     }
+
     private fun numberHandler(phoneNumber: CharSequence?) {
         var phone = phoneNumber.toString()
         phone = phone.replace(" ", "")
 
-        if(phone.startsWith("05"))
-            phone = "966" + phone.substring(phone.indexOf("05")+1)
-        else if(phone.startsWith("+"))
-            phone = phone.substring(phone.indexOf("+")+1)
+        if (phone.startsWith("05"))
+            phone = "966" + phone.substring(phone.indexOf("05") + 1)
+        else if (phone.startsWith("+"))
+            phone = phone.substring(phone.indexOf("+") + 1)
 
-        switchToApp(phone)
+        val saudiNumberRegex = "((\\+)?(966([_ \\-])?|0)5[0-9]{8})"
+        val isASaudiPhoneNumber = phone.contains(Regex(saudiNumberRegex))
+
+        if (isASaudiPhoneNumber) {
+            switchToApp(phone)
+        } else {
+            Toast.makeText(this, "Not a Saudi Number", Toast.LENGTH_SHORT).show()
+            onStop()
+        }
     }
 
     private fun switchToApp(cleanPhone: String) {
