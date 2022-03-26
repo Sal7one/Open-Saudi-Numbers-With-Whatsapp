31385037e43559cf08d10dade5b24bd512992a94
Sal7one <Alanazi.saleh.77@gmail.com>
2022-03-26 19:39:00 +0300

    theme work

--- app/src/main/java/com/example/numberswitcher/ui/theme/Color.kt
+++ app/src/main/java/com/example/numberswitcher/ui/theme/Color.kt
@@ -6,3 +6,5 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val dialog_background_color = Color(0xC6C256DA)
