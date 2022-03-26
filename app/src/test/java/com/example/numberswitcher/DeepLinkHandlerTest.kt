8da9ec30e75a00d92fb69863ccd09972e5f78ce5
Sal7one <Alanazi.saleh.77@gmail.com>
2022-03-26 19:43:37 +0300

    added test data

--- app/src/test/java/com/example/numberswitcher/DeepLinkHandlerTest.kt
+++ app/src/test/java/com/example/numberswitcher/DeepLinkHandlerTest.kt
@@ -27,4 +27,33 @@ class DeepLinkHandlerTest {
     private lateinit var multiNumsWithText1: String
     private lateinit var multiNumsWithText2: String
 
+    @Before
+    fun setUp() {
+        // Mobile
+        numberWithoutPlusAndCountryCode = "0541234567"
+        numberWithoutPlus = "966541234567"
+        numberWithPlusAndCountryCode = "+966541234567"
+
+        // Telephone
+        telephoneWithoutPlusAndCountryCode = "0125876524"
+        telephoneWithoutPlus = "9660125876524"
+        telephoneWithPlusAndCountryCode = "+966125876524"
+
+        // Numbers with text
+        numWithText1 = "Hello This is a stexct twekfwekisjf \n+966\n540-21-4\n5=1asd"
+        numWithText2 = "Hello This is a stexct twekfwekisjf sdfsdfsdfsdf+966-541234567aszf41saf6s-"
+        numWithText3 =
+            "Hello This is a stexct 966 541234567 twekfwekisjf sdfsdfsdfsdf+966-541234567aszf41saf6s-"
+        numWithText4 =
+            "Hello This is a stexct 966 0541234567 twekfwekisjf sdfsdfsdfsdf+966-541234567aszf41saf6s-"
+        numWithText5 =
+            "Hello This is a stexct +966 0541234567 twekfwekisjf sdfsdfsdfsdf+966-541234567aszf41saf6s-"
+        numWithText6 =
+            "Hello This is a stexct +966 541234567 twekfwekisjf sdfsdfsdfsdf+966-541234567aszf41saf6s-"
+
+        // Multiple numbers
+        multiNums = "+966541234567+966541234567+966541234567+966541234567"
+        multiNumsWithText1 = "fsdfsdf+966541234567fsdf\nfdsfdsf+966541234567sfsdfd"
+        multiNumsWithText2 = "sdfsdf\n0540885465sddsfsdf\n0541234567sadasd"
+    }
 }
