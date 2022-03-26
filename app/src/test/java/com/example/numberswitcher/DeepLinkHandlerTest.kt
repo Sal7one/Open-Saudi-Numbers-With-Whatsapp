0d828386684bcba750edd3c1374a9fc694dfef78
Sal7one <Alanazi.saleh.77@gmail.com>
2022-03-26 19:44:30 +0300

    added functions to be unit tested

--- app/src/test/java/com/example/numberswitcher/DeepLinkHandlerTest.kt
+++ app/src/test/java/com/example/numberswitcher/DeepLinkHandlerTest.kt
@@ -56,4 +56,48 @@ class DeepLinkHandlerTest {
         multiNumsWithText1 = "fsdfsdf+966541234567fsdf\nfdsfdsf+966541234567sfsdfd"
         multiNumsWithText2 = "sdfsdf\n0540885465sddsfsdf\n0541234567sadasd"
     }
+
+    private fun getListOfNumbers(numberWithoutText: String): List<String> {
+        val regex = Regex("((\\+)?(966([_ \\-])?0?|0)(5[0-9]{8}|1([1-9])[0-9]{7}))")
+        val matches = regex.findAll(numberWithoutText)
+        // Group value 0 Matches all of the Regex pattern
+        val stringOfNumbers = matches.map { it.groupValues[0] }.joinToString()
+
+        // Split the string into List
+        return stringOfNumbers.split(",").toList()
+    }
+
+    private fun formatNumber(number: String): String {
+        var telNumber = number.replace(" ", "")
+        when {
+            // Edge case of +966 with 05 or 01 --> +966050 or +966013
+            telNumber.startsWith("+96605") -> telNumber =
+                "966" + telNumber.substring(telNumber.indexOf("05") + 1)
+            telNumber.startsWith("+96601") -> telNumber =
+                "966" + telNumber.substring(telNumber.indexOf("01") + 1)
+            // Edge case of 966 with 05 or 01 --> 966050 or 966013
+            telNumber.startsWith("96605") -> telNumber =
+                "966" + telNumber.substring(telNumber.indexOf("05") + 1)
+            telNumber.startsWith("96601") -> telNumber =
+                "966" + telNumber.substring(telNumber.indexOf("01") + 1)
+            // Mobile numbers
+            telNumber.startsWith("05") -> telNumber =
+                "966" + telNumber.substring(telNumber.indexOf("05") + 1)
+            // Line/Telephone numbers
+            telNumber.startsWith("01") -> telNumber =
+                "966" + telNumber.substring(telNumber.indexOf("01") + 1)
+            // Doesn't matter just remove +
+            telNumber.startsWith("+") -> telNumber = telNumber.substring(telNumber.indexOf("+") + 1)
+        }
+        return telNumber
+    }
+
+    private fun removeTextFromString(numberWithText: String): String {
+        return numberWithText.replace(regex = Regex("([^(+)?0-9])"), "")
+    }
+
+    private fun isASaudiPhoneNumber(phone: String): Boolean {
+        val saudiNumberRegex = "((\\+)?(966([_ \\-])?|0)(5[0-9]{8}|1([1-9])[0-9]{7}))"
+        return phone.contains(Regex(saudiNumberRegex))
+    }
 }
