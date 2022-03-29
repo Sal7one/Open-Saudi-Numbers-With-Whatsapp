75374ca17a5f2adf9a2d09f528ac73c96a86158f
Sal7one <Alanazi.saleh.77@gmail.com>
2022-03-29 13:58:14 +0300

    still working on country picker

--- app/src/main/java/com/example/numberswitcher/CountrySelector.kt
+++ app/src/main/java/com/example/numberswitcher/CountrySelector.kt
@@ -1,45 +1,33 @@
 package com.example.numberswitcher
 
-import android.app.Activity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import com.example.numberswitcher.ui.theme.dialog_background_color
 
 @Composable
 fun CountrySelector(
-    clickedNumber: (String) -> Unit,
 ) {
-    val activity = (LocalContext.current as? Activity)
-
-    Dialog(onDismissRequest = { activity?.finish() }) {
-        Card(
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier.padding(10.dp, 5.dp, 10.dp, 10.dp),
-            elevation = 8.dp
-        ) {
-            Column(
-                Modifier
-                    .background(Color.White)
-            ) {
+    Card(modifier = Modifier.size(500.dp)) {
+        Column {
+            Text(text = "Country Picker")
+            Spacer(modifier = Modifier.height(50.dp))
+            Row{
+            Text(text = "Country code")
+                TextField(value = "", onValueChange = {}, placeholder = { Text("+966") }  )
+                Spacer(modifier = Modifier.width(30.dp))
+                TextField(value = "", onValueChange = {},placeholder = { Text("550123456") }  )
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CountrySelector_PREVIEW(){
+    CountrySelector()
 }
